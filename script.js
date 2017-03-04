8d7b9b792124e2e6a357fc0571331de070c15284
Pamela Anderson <pamviolet.a@gmail.com>
2017-03-03 16:52:32 -0800

    add contact links

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ $(function() {
   $(document.body).on("appear", ".block-container", function(e, $affected) {
     let section = $affected[0].className.split(" ")[1];
     $(".nav[name=" + section + "]")[0].className = "nav toggled"
-    console.log('app');
   });
 
   $(document.body).on("disappear", ".block-container", function(e, $affected) {
-      console.log('dis');
     let section = $affected[0].className.split(" ")[1];
     $(".nav[name=" + section + "]")[0].className = "nav"
   });
